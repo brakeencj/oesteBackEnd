430ecd28c3bf753a539689fbbb97e2a576ccecb5
Gabriel Cervantes <brakeencj@gmail.com>
2021-04-22 18:42:13 -0500

    Edición en la configuración de Swagger

--- src/main/java/com/oeste/evaluacion/config/SwaggerConfig.java
+++ src/main/java/com/oeste/evaluacion/config/SwaggerConfig.java
@@ -16,8 +16,9 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.openwebinars.net.rest.controller"))
+				.apis(RequestHandlerSelectors.basePackage("com.oeste.evaluacion.controller"))
 				.paths(PathSelectors.any())
+				// .paths(regex("/api.*"))
 				.build();
 	}
 }
